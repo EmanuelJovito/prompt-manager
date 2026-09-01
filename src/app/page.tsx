14b0cc7bca8ca1ee7ebaca58fd0f23d876727b64
emanuel <emanuel_carneiro@hotmail.com>
2026-09-01 16:55:55 -0300

    feat: configurando prettier, eslint e lefthook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div><h1>Rocketseat</h1></div>
+    <div>
+      <h1>Rocketseat</h1>
+    </div>
   );
 }
